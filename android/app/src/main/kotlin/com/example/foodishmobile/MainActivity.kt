012dafd72684e73f9d43cecca10671827dc9c95e
Adam Badruzzaman <94296882+adambadruzz@users.noreply.github.com>
2023-07-26 09:45:58 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/foodishmobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.foodishmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:foodishmobile/view/splash/splashscreen.dart';
+
+import 'view/home/home.dart';
+import 'view/profile/profile.dart';
+
+import 'navigator/drawer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
